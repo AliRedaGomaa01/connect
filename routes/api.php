141c1502655503822e765d8ee16b8cfe5f5909c4
Ali Reda <alyredagomaa@gmail.com>
2024-02-23 13:13:34 +0200

    fix the bug in api

--- routes/api.php
+++ routes/api.php
@@ -67,7 +67,7 @@ Route::name('api.v1.')->middleware('lang')->prefix('v1')->group( function () {
         # users
         Route::get('users/{id}/images',[ImageController::class, 'index'])->name('users.images');
         Route::get('users/{id}/works',[WorkController::class, 'index'])->name('users.works');
-        Route::get('users/{id}/followers/{status?}',[UserController::class, 'index'])->name('follows');  // status ['following' , 'followed']
+        Route::get('users/{id}/followers/{status?}',[UserController::class, 'index'])->name('users.follows');  // status ['following' , 'followed']
         Route::controller(UserController::class)->group(function () {
             Route::get('users/search', 'search')->name('users.search');
             Route::post('users/search-result', 'searchResult')->name('users.search.result');
