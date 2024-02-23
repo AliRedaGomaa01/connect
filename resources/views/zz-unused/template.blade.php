726785dbe2232df01b6e7e14ab2237a6639e1665
Ali Reda <alyredagomaa@gmail.com>
2024-02-23 11:55:44 +0200

    chore :  (( small edits ))

--- resources/views/zz-unused/template.blade.php
+++ resources/views/zz-unused/template.blade.php
@@ -4,14 +4,45 @@
 {{-- ****************************************************** --}}
 {{-- one time used php variables --}}
 @php
-    # lang
-    $ex1En = [
-        'key' => 'value' ,
-    ];
-    $ex1Ar = [
-        'key' => 'القيمة' ,
-    ];
-    $ex1 = app()->isLocale('ar') ?  $ex1Ar : $ex1En;
-    # others
+    // # lang
+    // $ex1En = [
+    //     'key' => 'value' ,
+    // ];
+    // $ex1Ar = [
+    //     'key' => 'القيمة' ,
+    // ];
+    // $ex1 = app()->isLocale('ar') ?  $ex1Ar : $ex1En;
+    // # others
 @endphp
 {{-- ****************************************************** --}}
+<?php 
+
+    /**
+     * ************************ Api Template ************************
+     */
+    // // testing
+    // return response()->json('test');
+    // // validation process
+    // $rules = [
+    //     'category' => ['required','string','max:255'],
+    //     'title' => ['required','string','max:255'],
+    //     'description' => ['required','string','max:2047'],
+    //     'url' => ['required','string', 'url:http,https' ,'max:255'],
+    // ];
+    // $validator = Validator::make($request->all(), $rules);
+    // if ($validator->fails()) {
+    //     $errors =  $validator->errors();
+    //     $data= [
+    //         'status' => false,
+    //         'errors' => $errors
+    //     ];
+    //     return response()->json($data) ;
+    // }
+    // $validated = $validator->safe()->all();
+
+    // // return response
+    // $data= [
+    //     'status' => true,
+    //     'data' => compact('') 
+    // ];
+    // return response()->json($data);
